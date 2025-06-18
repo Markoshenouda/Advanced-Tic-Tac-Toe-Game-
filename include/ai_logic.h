bf7cdc10efbea223c8be372ab0ce726966b07683
marko shenouda <136264452+Markoshenouda@users.noreply.github.com>
2025-06-18 16:18:26 +0300

    Update ai_logic.h

--- include/ai_logic.h
+++ include/ai_logic.h
@@ -1,9 +1,4 @@
-/*
- * ai_logic.h
- *
- *  Created on: Apr 28, 2025
- *      Author: Mariam Samy
- */
+
 
 #ifndef AI_LOGIC_H
 #define AI_LOGIC_H
@@ -18,9 +13,9 @@ public:
     static void mediumAI(const std::vector<char>& board, std::vector<int>& used, int& choice,
                         char aiSymbol, char playerSymbol);
     static void hardAI(const std::vector<char>& board, std::vector<int>& used, int& choice, char aiSymbol);
+    static int minimax(std::vector<char> board, bool isMaximizing, char aiSymbol, char playerSymbol);
 
 private:
-    static int minimax(std::vector<char> board, bool isMaximizing, char aiSymbol, char playerSymbol);
 };
 
 #endif
